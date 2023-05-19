484da03f471d0c933ea3d3b1d18c8d0059165c1f
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-05-19 23:18:17 +0000

    Update 5 files

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -1,4 +1,30 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
 module.exports = {
-  solidity: "0.8.18",
+ solidity: {
+   version: "0.8.18",
+   settings: {
+     optimizer: {
+       enabled: true,
+       runs: 200,
+     },
+   },
+ },
+ networks: {
+   hardhat: {},
+   ethereum: {
+     url: process.env.MAINNET_RPC,
+     accounts: [process.env.MNEMONIC],
+   },
+   sepolia: {
+     url: process.env.SEPOLIA_RPC,
+     accounts: [process.env.MNEMONIC],
+   },
+ },
 };
