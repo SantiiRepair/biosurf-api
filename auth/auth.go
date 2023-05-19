46276715e2e715bc8e96f0f651c25b88ad9f0f52
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-05-19 06:03:19 +0000

    Update auth.go and main.go

--- auth/auth.go
+++ auth/auth.go
@@ -4,14 +4,7 @@ import (
 	gin "github.com/gin-gonic/gin"
 )
 
-func main() {
-	r := gin.Default()
-
+func Routes(r *gin.Engine) {
 	r.POST("/register", HandleRegister)
 	r.POST("/login", HandleLogin)
-
-	err := r.Run(":8080")
-	if err != nil {
-		panic(err)
-	}
 }
--- main.go
+++ main.go
@@ -1,2 +1,14 @@
 package main
 
+import (
+	"github.com/SantiiRepair/biosurf-api/auth"
+	gin "github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	auth.Routes(r)
+
+	r.Run(":8080")
+}
