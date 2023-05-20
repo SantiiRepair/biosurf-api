2f0e981a066d92870e67e7a9f23f433ed8f7dad2
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-05-20 01:10:29 +0000

    Update profanity.go

--- report/profanity.go
+++ report/profanity.go
@@ -10,9 +10,9 @@ func HandleReport(c *gin.Context) {
 	text := c.PostForm("text")
 
 	if isProfanity(text) {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "The text contains an obscene word"})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "The text contains an obscene word"})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"mensaje": "The text does not contain obscene words"})
+	c.JSON(http.StatusOK, gin.H{"message": "The text does not contain obscene words"})
 }
