7714ffb59c5c266bcf915b40d3849b04dc84c7b2
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-06-11 18:19:19 +0000

    Update 3 files

--- dynamic/mails/mails.go
+++ dynamic/mails/mails.go
@@ -1,1 +1,28 @@
 package mails
+
+import (
+	"fmt"
+	"net/smtp"
+)
+
+func EmailNotifier() {
+	from := "from@gmail.com"
+	password := "<Email Password>"
+
+	to := []string{
+		"sender@example.com",
+	}
+
+	host := "smtp.gmail.com"
+	port := "587"
+
+	message := []byte("This is a test email message.")
+
+	auth := smtp.PlainAuth("", from, password, host)
+
+	err := smtp.SendMail(host+":"+port, auth, from, to, message)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
