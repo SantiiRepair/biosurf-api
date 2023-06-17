c46b6d62f81644cf3b96a0846bd1bcc893505e8b
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-06-17 03:13:09 +0000

    Update google.go

--- auth/user/google.go
+++ auth/user/google.go
@@ -1,13 +1,34 @@
 package user
 
-import "google.golang.org/api/oauth2/v2"
+import (
+	"net/http"
 
-func getTokenInfo(idToken string) (*oauth2.Tokeninfo, error) {
-oauth2Service,err := oauth2.New(&http.Client{})
-if err != nil {
-    return nil, err
-}
-tokenInfoCall := oauth2Service.Tokeninfo()
-tokenInfoCall.IdToken(idToken)
-return tokenInfoCall.Do()
+	fiber "github.com/gofiber/fiber/v2"
+	"google.golang.org/api/oauth2/v2"
+)
+
+func HandleGoogle(c *fiber.Ctx) (*oauth2.Tokeninfo, error) {
+	idToken := c.FormValue("googleToken")
+	oauth2Service, err := oauth2.New(&http.Client{})
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return nil, c.JSON(fiber.Map{
+			"message": "Could not login",
+		})
+	}
+
+	tokenInfoCall := oauth2Service.Tokeninfo()
+	tokenInfoCall.IdToken(idToken)
+	decode, err := tokenInfoCall.Do()
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return nil, c.JSON(fiber.Map{
+			"message": "Could not a",
+		})
+	}
+
+	c.Status(fiber.StatusOK)
+	return nil, c.JSON(fiber.Map{
+		"message": decode,
+	})
 }
