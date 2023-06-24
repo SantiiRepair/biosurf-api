cbb16e9f47db4469f3aab0793ef6b8dd571d6342
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-06-24 01:56:57 +0000

    hardhat compile

--- contracts/contracts/ERC721Proxy.sol
+++ contracts/contracts/ERC721Proxy.sol
@@ -4,6 +4,41 @@ pragma solidity ^0.8.0;
 
 import "./ERC721.sol";
 
-contract ERC721Proxy {
+contract ProxyProtocol {
+    address private _admin;
+    mapping(address => bool) private _userCreatedContract;
+    address[] private _createdContracts;
 
+    constructor() {
+        _admin = msg.sender;
+    }
+-
+    function implementation() public returns (address) {
+        require(
+            !_userCreatedContract[msg.sender],
+            "User has already created a contract"
+        );
+        address INEW = address(
+            new SMSUANCES("SMSUANCES", "SMS", _admin, msg.sender)
+        );
+        _createdContracts.push(INEW);
+        _userCreatedContract[msg.sender] = true;
+        return INEW;
+    }
+
+    function getContractCounts() public view returns (uint256) {
+        return _createdContracts.length;
+    }
+
+    function getContractAddresses() public view returns (address[] memory) {
+        return _createdContracts;
+    }
+
+    function getSmsuancesBalance(
+        address userAddress,
+        address nftContractAddress
+    ) public view returns (uint256) {
+        ERC721 nftContract = ERC721(nftContractAddress);
+        return nftContract.balanceOf(userAddress);
+    }
 }
